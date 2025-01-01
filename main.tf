53fa17ac31237e4401f419fc709bc5e6e9a92a35
SalikeHassan <salikehassan93@gmail.com>
2025-01-01 20:43:48 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">=1.10.3"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.43.0"
+    }
+  }
+  cloud {
+    organization = "hassan-tf-demo"
+    workspaces {
+      name = "TerraformCI"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-demo"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "stgrdemo"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+}
