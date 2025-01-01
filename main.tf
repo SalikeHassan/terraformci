5e49a51d2a2894785d8a8c5fa4914247984e107e
SalikeHassan <salikehassan93@gmail.com>
2025-01-01 20:59:40 +0530

    second storage

--- main.tf
+++ main.tf
@@ -33,3 +33,11 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "RAGRS"
 }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "stgrdemo2"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+}
